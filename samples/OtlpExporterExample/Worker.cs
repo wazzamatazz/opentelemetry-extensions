003496910ef20a8373bd9db04f845a1a3fe1b105
Graham Watts <grahamcharleswatts@gmail.com>
2024-09-27 10:26:52 +0300

    Add sample projects
    Adds sample projects demonstrating how to configure a default OTLP exporter and multiple OTLP exporter instances.

---
+++ samples/OtlpExporterExample/Worker.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace OtlpExporterExample {
+    public class Worker : BackgroundService {
+        public const string ActivitySourceName = "OtlpExporterExample";
+
+        private readonly ILogger<Worker> _logger;
+
+        private readonly ActivitySource _activitySource = new ActivitySource(ActivitySourceName);
+
+
+        public Worker(ILogger<Worker> logger) {
+            _logger = logger;
+        }
+
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken) {
+            while (!stoppingToken.IsCancellationRequested) {
+                using var activity = _activitySource.StartActivity("WorkerActivity");
+
+                if (_logger.IsEnabled(LogLevel.Information)) {
+                    _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                }
+
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
